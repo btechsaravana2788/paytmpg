bfc0e86ccf2e3b9aad7fd20086a457ed2381571c
btechsaravana2788 <133357804+btechsaravana2788@users.noreply.github.com>
2023-05-12 17:07:39 +0530

    Add files via upload

---
+++ paytmpg/api/responseapi.php
@@ -0,0 +1,16 @@
+<section id="wallet-section">
+    <div class="container">
+        <div class="page-title">
+            <h1>Response Page</h1>
+        </div>
+		<div class="row">
+            <div class="col-md-6 text-left">
+                <h5 class="wallet-h5">Response</h5>
+			</div>
+			<?php
+				print_r($_POST);
+				print_r($_REQUEST);
+			?>
+		</div>	
+	</div>
+</section>	
---
+++ paytmpg/footer.php
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+<div class="insert-post-ads1" style="margin-top:20px;">
+
+</div>
+</div>
+</body></html>
+
+=======
+<div class="insert-post-ads1" style="margin-top:20px;">
+
+</div>
+</div>
+</body></html>
+
+>>>>>>> f053b861482ed7d0ad59f557d4249cb5e1512e81
